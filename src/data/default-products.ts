59bc28d6344010a676d3ec1b55417c35d6490403
William Muli <willi.wambu@gmail.com>
2024-01-12 15:31:36 +0300

    Created default products for testing purposes

---
+++ src/data/default-products.ts
@@ -0,0 +1,7 @@
+import { Product } from '@/products/entities/product.entity';
+
+export const defaultProducts: Product[] = [
+  { name: 'Coke', price: 25, quantity: 10 },
+  { name: 'Pepsi', price: 35, quantity: 10 },
+  { name: 'Soda', price: 45, quantity: 10 },
+];
