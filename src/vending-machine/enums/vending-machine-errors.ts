fbaa4386493a2b7c86f86eb5f19dd03a10da5e0a
William Muli <willi.wambu@gmail.com>
2024-01-12 15:35:49 +0300

    Added an enum for vending machine buy errors

---
+++ src/vending-machine/enums/vending-machine-errors.ts
@@ -0,0 +1,6 @@
+export enum VendingMachineErrors {
+  InsufficientChange = 'Insufficient change',
+  InsufficientCoinsToReturnChange = 'Insufficient coins to return change',
+  ProductNotFound = 'Product not found',
+  InsufficientMoneyInserted = 'Insufficient money inserted',
+}
